a7de818548e50e7f379d562db3231ab37d5d75f3
aaminur <aamin.aminur@jodopay.com>
2022-09-19 11:12:18 +0530

    increased length

--- product_api/api/models.py
+++ product_api/api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Products(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     category = models.CharField(max_length=100, null=False, blank=False)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
     rating = models.IntegerField()
     
