b2844a00e602d33ec2ad4cb5c6d99da28cc5a65d
aaminur <aamin.aminur@jodopay.com>
2022-10-08 02:31:04 +0530

    Working code

--- product_api/api/urls.py
+++ product_api/api/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('/', views.apiOverview, name='api-overview'),
-    path('list/', views.showProducts, name='list'),
+    path("/", views.apiOverview, name="api-overview"),
+    path("list/", views.showProducts, name="list"),
+    path("list-one/<int:pk>/", views.showProduct, name="list-one"),
+    path("item-create", views.createProduct, name="item-create"),
+    path("item-update/<int:pk>/", views.updateProduct, name="item-update"),
+    path("item-delete/<int:pk>/", views.deleteProduct, name="item-delete"),
 ]
--- product_api/api/views.py
+++ product_api/api/views.py
@@ -1,3 +1,4 @@
+from functools import partial
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -6,19 +7,58 @@ from .models import Products
 
 # Create your views here.
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def apiOverview(request):
-    api_urls= {
-        'list':'/product-list/',
-        'detail_view':'/product-detail/<int:id>',
-        'create':'/product-create',
-        'update':'/product-update/<int:id>',
-        'delete':'/product-delete/<int:id>',
+    api_urls = {
+        "list": "/product-list/",
+        "detail_view": "/product-detail/<int:id>",
+        "create": "/product-create",
+        "update": "/product-update/<int:id>",
+        "delete": "/product-delete/<int:id>",
     }
     return Response(api_urls)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def showProducts(request):
     products = Products.objects.all()
     ser = ProductsSerializer(products, many=True)
     return Response(ser.data)
+
+
+@api_view(["GET"])
+def showProduct(request, pk):
+    product = Products.objects.get(id=pk)
+    ser = ProductsSerializer(product, many=False)
+    return Response(ser.data)
+
+
+@api_view(["POST"])
+def createProduct(request):
+    ser = ProductsSerializer(data=request.data)
+    if ser.is_valid():
+        ser.save()
+    return Response(ser.data)
+
+
+@api_view(["PATCH"])
+def updateProduct(request, pk):
+    product = Products.objects.get(id=pk)
+    if product:
+        ser = ProductsSerializer(instance=product, data=request.data, partial=True)
+        if ser.is_valid():
+            ser.save()
+        return Response(ser.data)
+    else:
+        return Response(product)
+
+
+@api_view(["GET"])
+def deleteProduct(request, pk):
+    product = Products.objects.get(id=pk)
+    if product:
+        product.delete()
+        return Response("Item deleted successfully")
+    else:
+        return Response("item not found")
